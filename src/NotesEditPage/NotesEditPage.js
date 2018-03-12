0c4b9fa3a96ab2a2e06739c177eac1549c1aadde
whybazinga <aom9@mail.ru>
2018-03-12 19:50:54 +0300

    added note editor page

--- src/NotesEditPage/NotesEditPage.js
+++ src/NotesEditPage/NotesEditPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 import '../notes.css';
 import './notes-edit.css';
 
@@ -6,11 +7,29 @@ class NotesEditPage extends React.Component {
   constructor() {
     super();
     this.username = 'Simon';
+    this.content = '';
+    this.saveNote = this.saveNote.bind(this);
+    this.backToMenu = this.backToMenu.bind(this);
+    this.handleEditorChange = this.handleEditorChange.bind(this);
+  }
+
+  backToMenu() {
+    console.log('back');
+  }
+
+  handleEditorChange(e) {
+    this.content = e.target.getContent();
+  }
+
+  saveNote(e) {
+    e.preventDefault();
+    console.log(this.content);
+    console.log('save me pls');
   }
 
   render() {
     return (
-      <main id="notes-edit-page" onLoad="">
+      <main id="notes-edit-page">
         <div id="notes-remarkable-header">
           <h1>REMARKABLE</h1>
           <div className="notes-header-user">
@@ -18,16 +37,39 @@ class NotesEditPage extends React.Component {
             <div className="notes-logout button">Log out</div>
           </div>
         </div>
-        <div id="notes-edit-main">
+        <form id="notes-edit-main" method="post" onSubmit={this.saveNote}>
           <div id="working-area">
             <input type="text" name="title" placeholder="Header" />
-            <textarea name="notes-edit-textarea" id="notes-edit-textarea" />
+            <Editor
+              init={{
+                plugins: 'link image code',
+                toolbar:
+                  'undo redo | bold italic | alignleft aligncenter alignright | code',
+                branding: false,
+                theme: 'modern',
+                resize: true,
+                height: '50vh',
+                mobile: {
+                  theme: 'mobile',
+                },
+              }}
+              onChange={this.handleEditorChange}
+            />
           </div>
           <div className="notes-edit-main-button-bar">
-            <button className="notes-edit-save button">Save</button>
-            <button className="notes-edit-back button">Menu</button>
+            <input
+              type="submit"
+              value="Save"
+              className="notes-edit-save button"
+            />
+            <button
+              className="notes-edit-back button"
+              onClick={this.backToMenu}
+            >
+              Menu
+            </button>
           </div>
-        </div>
+        </form>
       </main>
     );
   }
